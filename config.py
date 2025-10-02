b4dd34c2b10b7c5ca4f4341af4c5f0509e9578e9
sLimLong <slimlong103@yandex.ru>
2025-10-02 13:15:59 +0300

    1

---
+++ config.py
@@ -0,0 +1,62 @@
+# config.py
+
+TOKEN = ""
+REQUEST_INTERVAL = 60  # секунд
+
+ALLOWED_ADMINS = []
+
+BANLIST_GROUP_ID = 
+BANLIST_THREAD_ID = 
+
+GROUP_CHAT_ID = 
+CHANNEL_CHAT_ID =
+
+TICKET_GROUP_ID = 
+TICKET_THREAD_ID = 
+
+# 🔐 Конфигурация токенов и ID
+VK_TOKEN = ""
+VK_GROUP_ID =
+TELEGRAM_GROUP_ID = 
+TARGET_THREAD_ID = 
+
+WARGM_API_KEY = ''
+ITEM_IDS = []
+
+
+SERVERS = [
+    {
+        "name": "7DTD PVE 1",
+        "url": "",
+        "auth": ("", ""),
+        "channel_id": ,
+        'offer_id': '',
+        'server_id': ''
+    },
+    {
+        "name": "7DTD PVE 2",
+        "url": "",
+        "auth": ("", ""),
+        "channel_id": ,
+        'offer_id': '',
+        'server_id': ''
+    }
+]
+
+SERVERSRCON = [
+    {
+        "name": "7DTD PVE 1",
+        "host": "",
+        "port": ,
+        "password": ""
+    },
+    {
+        "name": "7DTD PVE 2",
+        "host": "",
+        "port": ,
+        "password": ""
+    }   
+]
+
+# Хранилище времени последнего запроса
+last_request_time = {}
